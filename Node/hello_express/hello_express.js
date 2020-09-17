4c8556f99f84828c67ffccd2b827cbd038e4f230
iagovital <58228760+iagovital@users.noreply.github.com>
2020-09-17 15:26:35 -0300

    Exercicios express

---
+++ Node/hello_express/hello_express.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+  console.log("Tudo OK! Servidor rodando na porta 3000");
+});
